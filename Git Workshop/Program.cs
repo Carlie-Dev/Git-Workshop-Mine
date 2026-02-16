5940907c25f23dbf55918cbc12a876430c75071a
Paul Bute <pb0986738@otc.edu>
2026-02-16 16:23:50 -0600

    Added pretty colors to the fizz's and buzz's!

--- Git Workshop/Program.cs
+++ Git Workshop/Program.cs
@@ -9,13 +9,13 @@
                 string fizzbuzz_out = "";
                 if ((count % 3 == 0) && (count % 5 == 0))
                 {
-                    fizzbuzz_out = "Fizzbuzz";
+                    fizzbuzz_out = "\x1b[35mFizzbuzz\x1b[39m";
                 } else if ((count % 3 == 0))
                 {
-                    fizzbuzz_out = "Fizz";
+                    fizzbuzz_out = "\x1b[31mFizz\x1b[39m";
                 }  if ((count % 5 == 0))
                 {
-                    fizzbuzz_out = "Buzz";
+                    fizzbuzz_out = "\x1b[34mBuzz\x1b[39m";
                 }
                 Console.WriteLine($"{count}! {fizzbuzz_out}");
                 Thread.Sleep(500);
